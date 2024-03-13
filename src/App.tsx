b24f1d4c65bc91cb2a30adc63b1f9c6af2ee17fa
msidorov <mikxsid@gmail.com>
2024-03-13 10:55:13 +0300

    add basename for router

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,28 @@ import Root from './routes/Root';
 import AboutPage from './components/AboutPage';
 import QuotesPage from './components/QuotesPage';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: 'about',
+          element: <AboutPage />,
+        },
+        {
+          path: 'quotes',
+          element: <QuotesPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-      {
-        path: 'quotes',
-        element: <QuotesPage />,
-      },
-    ],
-  },
-]);
+    basename: '/price-pulse-spa/',
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
